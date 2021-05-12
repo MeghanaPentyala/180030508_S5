00d15af08aa7e004cde71c57f222b08922bbd9d2
pentyala <pentyala@DESKTOP-LSM7JP5>
2021-05-12 12:02:44 +0530

    First Commit

---
+++ Loops1/src/loops/problem13.java
@@ -0,0 +1,22 @@
+package loops;
+
+
+public class problem13 {
+	public static void main(String[] args) {
+	    for (int i = 10; i < 99; ++i) {
+	      boolean isPrime = true;
+	      int sqrt = (int)Math.ceil(Math.sqrt(i));
+	      for (int divisor = 2; divisor <= sqrt; ++divisor) {
+	        if (i % divisor == 0) {
+	          isPrime = false;
+	          break;
+	        }
+	      }
+	      if (isPrime) {
+	        System.out.println(i);
+	      }
+	    }
+	}
+}
+
+
---
+++ Loops1/src/loops/problem7.java
@@ -0,0 +1,23 @@
+package loops;
+import java.util.*;
+
+public class problem7 {
+	public static void main(String[] input)
+    {
+        char ch;
+        int temp;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Lowercase : ");
+        ch = sc.next().charAt(0);
+		
+        temp = (int) ch;
+        temp = temp - 32;
+        ch = (char) temp;
+		
+        System.out.print("Uppercase = " +ch);
+        sc.close();
+    }
+}
+
+
